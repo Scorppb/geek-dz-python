24c1c11b8116ebbb3006ddbfadb497b65ed94a95
Scorppb <scorppb@yandex.ru>
2022-09-23 19:34:21 +0300

    dz 01 tsk 02

--- 01/02.py
+++ 01/02.py
@@ -2,42 +2,18 @@
 # для всех значений предикат.
 
 
+def logical_statement(x, y, z):
+    print(f"¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} is {(not (x or y or z)) == (not x and not y and not z)}")
+    return (not (x or y or z)) == (not x and not y and not z)
 
-1.
-x = int(input('Введите число x '))
-y = int(input('Введите число y '))
-z = int(input('Введите число z '))
-
-a = x * y * z
-b = x + y + z
-
-if a > 0:
-    a = 0
-elif a < 1:
-    a = 1
-if b > 0:
-    b = 1
-elif b < 1:
-    b = 1
-
-if a == b:
-    print('Утверждение истинно')
-elif a != b:
-    print('Утверждение ложно')
-
-leftSide = not (x or y or z)
-rightSide = not x and not y and not z
-result = leftSide == rightSide
-
-if result == True:
-    print('Утверждение истинно')
+if (logical_statement(0, 0, 0) 
+and logical_statement(0, 0, 1) 
+and logical_statement(0, 1, 0) 
+and logical_statement(0, 1, 1) 
+and logical_statement(1, 0, 0) 
+and logical_statement(1, 0, 1) 
+and logical_statement(1, 1, 0) 
+and logical_statement(1, 1, 1)):
+    print("Истинно")
 else:
-    print('Утверждение ложно')
-
-
-#Идеальное рещение
-for x in range(2):
-        for y in range(2):
-            for z in range(2):
-                print(not (x or y or z) == (not x and not y and not z))
-                print(x, y, z)
+    print("Ложно")
