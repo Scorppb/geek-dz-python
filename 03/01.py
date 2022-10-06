d7fc182d04f67f4efbc373ade6f6cf2896ee10f5
Scorppb <scorppb@yandex.ru>
2022-10-06 17:46:05 +0300

    dz 04 tsk 05

---
+++ 03/01.py
@@ -0,0 +1,13 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, 
+# стоящих на нечётной позиции.
+# Пример:
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+import random
+number=int(input('Введите число> '))
+number_list=[]
+for i in range(number):
+   number_list.append(random.randint(1,10))
+print(number_list)
+print(sum(number_list[1::2]))
+
---
+++ 03/02.py
@@ -0,0 +1,18 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и 
+# последний элемент, второй и предпоследний и т.д.
+# Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+import random
+number=int(input('Введите число> '))
+number_list=[]
+for i in range(number):
+   number_list.append(random.randint(1,10))
+print(number_list)
+new_list= []
+for i in range((len(number_list)+1)//2):
+    new_list.append(number_list[i]*number_list[len(number_list)-1-i])
+print(new_list)
+
+
