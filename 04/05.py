841dc2bddc5e095209e1bc1f946be26023d59059
Scorppb <scorppb@yandex.ru>
2022-10-10 19:59:24 +0300

    Check errors dz 04

--- 04/05.py
+++ 04/05.py
@@ -1,4 +1,22 @@
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, 
 # содержащий сумму многочленов.
 
+with open('Polynomial.txt', 'w', encoding='utf-8') as file:
+    file.write('2*x^2 + 5*x^5')
+with open('Polynomial2.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^4 + 9*x^6')
 
+with open('Polynomial.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+
+
+with open('Polynomial2.txt','r') as file:
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+
+with open('Sum_polynomial.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
