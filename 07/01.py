e337746faa8f66769217d3d8d0916e52302b558f
Scorppb <scorppb@yandex.ru>
2022-11-07 16:56:10 +0300

    dz 07 phonebook

--- 07/01.py
+++
@@ -1,7 +0,0 @@
-def read_csv(filename: str) -> list:
-    data = []
-    fields = ["Фамилия", "Имя", "Телефон", "Описание"]
-    with open(filename, 'r', encoding='utf-8') as fin:
-        for line in fin:
-            record = dict(zip(fields, line.strip().split(',')))
-            data.append(record)
